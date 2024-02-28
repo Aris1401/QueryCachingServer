19376a7c71987c2f2b23e466dff77355821b1173
SOLOFOARISON Tolotry Ny Avo Arisaina <62713580+Aris1401@users.noreply.github.com>
2024-02-28 20:55:40 +0300

    reset 2

---
+++ src/main/java/com/sqlcaching/server/SqlCachingServerApplication.java
@@ -0,0 +1,13 @@
+package com.sqlcaching.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlCachingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlCachingServerApplication.class, args);
+	}
+
+}
